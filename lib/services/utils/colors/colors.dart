ee775e78af90d0f03096f15f5396faea2c895ff0
Jubayer <jubayer.montasir@apexdmit.com>
2024-08-26 14:20:33 +0600

    add colors for eo

---
+++ lib/services/utils/colors/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EoColors {
+  const EoColors();
+
+  //Core color
+
+  ///Base Color
+  final white = const Color(0xFFFFFFFF);
+  final black = const Color(0xFF000000);
+  final transparent = const Color(0x00000000);
+
+  final eoTertiaryColor = const Color(0xFF828282);
+
+  final eoUnSelectedTabBarColor = const Color(0xFFEEEFFD);
+  final eoUnSelectedBtnBgColor = const Color(0xFF848487);
+
+  final eoPrimaryColor = const Color(0xFF3239CB);
+  final eoSecondaryColor = const Color(0xFF5669FF);
+
+  final eoPrimaryTextColor = const Color(0xFF161616);
+  final eoSecondaryTextColor = const Color(0xFF606060);
+  final eoTextBoxBorderColor = const Color(0xFFE4DFDF);
+
+  final eoEditIconColor = const Color(0xFF8F8F8F);
+  final eoSignOutBtnBgColor = const Color(0xFFFFE5E5);
+  final eoSignOutIconColor = const Color(0xFFF2583E);
+
+}
